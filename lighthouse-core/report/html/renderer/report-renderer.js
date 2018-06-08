934e6daa0aaad9f4ffa1ad2dcf71a109a42c0734
Paul Irish <paulirish@google.com>
2018-06-08 11:54:32 -0700

    pause on click per @hwikyounglee

--- lighthouse-core/report/html/renderer/report-renderer.js
+++ lighthouse-core/report/html/renderer/report-renderer.js
@@ -187,7 +187,12 @@ class ReportRenderer {
 
     // Fireworks
     const scoresAll100 = report.reportCategories.every(cat => cat.score === 1);
-    if (scoresAll100) headerContainer.classList.add('score100');
+    if (scoresAll100) {
+      headerContainer.classList.add('score100');
+      this._dom.find('.lh-header', headerContainer).addEventListener('click', _ => {
+        headerContainer.classList.toggle('fireworks-paused');
+      });
+    }
 
     if (scoreHeader) {
       const scoreScale = this._dom.cloneTemplate('#tmpl-lh-scorescale', this._templateContext);
