0e6bf8a2b6593fbdc4e3b3e7bb03d5211f785987
Patrick Hulce <patrick.hulce@gmail.com>
2018-07-10 13:25:01 -0700

    core(i18n): initial utility library

---
+++ lighthouse-core/lib/locales/en-US.js
@@ -0,0 +1,40 @@
+/**
+* @license Copyright 2018 Google Inc. All Rights Reserved.
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+'use strict';
+
+// THIS FILE IS AUTO-GENERATED, DO NOT MODIFY
+
+/* eslint-disable */
+
+module.exports = {
+  "lighthouse-core/audits/byte-efficiency/render-blocking-resources.js!#title": {
+    "message": "Eliminate render-blocking resources"
+  },
+  "lighthouse-core/audits/byte-efficiency/render-blocking-resources.js!#description": {
+    "message": "Resources are blocking the first paint of your page. Consider delivering critical JS/CSS inline and deferring all non-critical JS/styles. [Learn more](https://developers.google.com/web/tools/lighthouse/audits/blocking-resources)."
+  },
+  "lighthouse-core/audits/byte-efficiency/render-blocking-resources.js!#displayValue": {
+    "message": "{itemCount, plural,\n    one {1 resource}\n    other {# resources}\n    } delayed first paint by {timeInMs, number, milliseconds} ms"
+  },
+  "lighthouse-core/audits/metrics/interactive.js!#title": {
+    "message": "Time to Interactive"
+  },
+  "lighthouse-core/audits/metrics/interactive.js!#description": {
+    "message": "Interactive marks the time at which the page is fully interactive. [Learn more](https://developers.google.com/web/tools/lighthouse/audits/consistently-interactive)."
+  },
+  "lighthouse-core/lib/i18n.js!#ms": {
+    "message": "{timeInMs, number, milliseconds} ms"
+  },
+  "lighthouse-core/lib/i18n.js!#columnURL": {
+    "message": "URL"
+  },
+  "lighthouse-core/lib/i18n.js!#columnSize": {
+    "message": "Size (KB)"
+  },
+  "lighthouse-core/lib/i18n.js!#columnWastedTime": {
+    "message": "Potential Savings (ms)"
+  }
+};
