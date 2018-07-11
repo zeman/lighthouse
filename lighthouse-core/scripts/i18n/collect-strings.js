f58423f9cabfefcd2f25018ffacbbe50a5709073
Patrick Hulce <patrick.hulce@gmail.com>
2018-07-11 10:47:34 -0700

    json locale files

--- lighthouse-core/scripts/i18n/collect-strings.js
+++ lighthouse-core/scripts/i18n/collect-strings.js
@@ -22,19 +22,6 @@ const ignoredPathComponents = [
   '-test.js',
 ];
 
-const localeHeaderContent = `/**
-* @license Copyright 2018 Google Inc. All Rights Reserved.
-* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-'use strict';
-
-// THIS FILE IS AUTO-GENERATED, DO NOT MODIFY
-
-/* eslint-disable */
-
-`;
-
 /**
  * @param {string} dir
  * @param {Record<string, string>} strings
@@ -75,9 +62,11 @@ function createPsuedoLocaleStrings(strings) {
       const char = string.substr(i, 1);
       psuedoLocalizedString.push(char);
       // Don't touch the characters inside braces
-      if (char === '{') braceCount++;
-      else if (char === '}') braceCount--;
-      else if (braceCount === 0) {
+      if (char === '{') {
+        braceCount++;
+      } else if (char === '}') {
+        braceCount--;
+      } else if (braceCount === 0) {
         if (/[a-z]/i.test(char)) {
           psuedoLocalizedString.push(useHatForAccentMark ? `\u0302` : `\u0301`);
           useHatForAccentMark = !useHatForAccentMark;
@@ -96,17 +85,13 @@ function createPsuedoLocaleStrings(strings) {
  * @param {Record<string, string>} strings
  */
 function writeStringsToLocaleFormat(locale, strings) {
-  const fullPath = path.join(LH_ROOT, 'lighthouse-core/lib/locales/', `${locale}.js`);
-  const outputObject = {};
+  const fullPath = path.join(LH_ROOT, `lighthouse-core/lib/locales/${locale}.json`);
+  const output = {};
   for (const [key, message] of Object.entries(strings)) {
-    outputObject[key] = {message};
+    output[key] = {message};
   }
 
-  const outputString = localeHeaderContent + `
-    module.exports = ${JSON.stringify(outputObject, null, 2)};
-  `.trim() + '\n';
-
-  fs.writeFileSync(fullPath, outputString);
+  fs.writeFileSync(fullPath, JSON.stringify(output, null, 2));
 }
 
 const strings = collectAllStringsInDir(path.join(LH_ROOT, 'lighthouse-core'));
