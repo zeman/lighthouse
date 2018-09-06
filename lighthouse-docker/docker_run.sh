46ceffd80628c0877bcdddc2bd416118fee845cd
Ewa Gasperowicz <devnook@gmail.com>
2018-09-06 17:16:44 +0200

    Remove dumb-init and use --init instead

--- lighthouse-docker/docker_run.sh
+++ lighthouse-docker/docker_run.sh
@@ -16,8 +16,8 @@
 
 if [ "$1" == "--headful" ]; then
   docker kill lighthouse_headful
-  docker run -dit -p 8080:8080 --rm --name lighthouse_headful --cap-add=SYS_ADMIN lighthouse_headful
+  docker run -dit -p 8080:8080 --init --rm --name lighthouse_headful --cap-add=SYS_ADMIN lighthouse_headful
 else
   docker kill lighthouse_docker
-  docker run -dit -p 8080:8080 --rm --name lighthouse_docker --cap-add=SYS_ADMIN lighthouse_docker
+  docker run -dit -p 8080:8080 --init --rm --name lighthouse_docker --cap-add=SYS_ADMIN lighthouse_docker
 fi
